e83eead56a9fb91fcd866de4f9a38c89374562c1
TheerapornBup <theeraporn.b@gmail.com>
2023-04-04 20:14:49 +0700

    update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDoJEa_FZnDC7FY_FH5TfmouIQpWVRY7tM",
-  authDomain: "crud-vue-1b5f4.firebaseapp.com",
-  projectId: "crud-vue-1b5f4",
-  storageBucket: "crud-vue-1b5f4.appspot.com",
-  messagingSenderId: "120371820005",
-  appId: "1:120371820005:web:8ce00e39b7659464056b0c",
-  measurementId: "G-0K1SFW2C63",
+  apiKey: process.env.VUE_APP_FIREBASE_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
